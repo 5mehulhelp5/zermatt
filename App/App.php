bf991a2e53549cbee34a04efca5a688da6532769
Hervé Guétin <herve.guetin@gmail.com>
2024-12-02 22:04:45 +0100

    Disable frontend for non-Zermatt themes

--- App/App.php
+++ App/App.php
@@ -5,8 +5,10 @@
 
 namespace Maddlen\Zermatt\App;
 
+use Exception;
 use Magento\Framework\Component\ComponentRegistrarInterface as ComponentRegistrar;
 use Magento\Framework\View\Asset\Repository;
+use Magento\Framework\View\Element\AbstractBlock;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class App implements ArgumentInterface
@@ -25,9 +27,14 @@ class App implements ArgumentInterface
     {
     }
 
-    public function sourceDir(): string
+    public function withZermatt(AbstractBlock $block): bool
     {
-        return $this->componentRegistrar->getPath('module', 'Maddlen_Zermatt') . self::SRC_DIR;
+        try {
+            $block->getViewFileUrl($this->entryFilepath());
+            return true;
+        } catch (Exception) {
+            return false;
+        }
     }
 
     public function entryFilepath(): string
@@ -35,5 +42,10 @@ class App implements ArgumentInterface
         $manifest = $this->assetRepo->createAsset(self::DIST_DIR . self::MANIFEST_FILEPATH);
         $manifestData = json_decode($manifest->getContent(), true, 512, JSON_THROW_ON_ERROR);
         return self::DIST_DIR . $manifestData['index.html']['file'];
+    }
+
+    public function sourceDir(): string
+    {
+        return $this->componentRegistrar->getPath('module', 'Maddlen_Zermatt') . self::SRC_DIR;
     }
 }
