245b5e9c8257cb95745df7ee5e9d4187bd5c736d
Hervé Guétin <herve.guetin@gmail.com>
2024-12-04 23:30:12 +0100

    Added SASS support
    Added x-cloak to zermatt.scss
    
    Licence: MIT non commercial

--- view/frontend/web/zermatt/vite.config.js
+++ view/frontend/web/zermatt/vite.config.js
@@ -1,7 +1,33 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
+import zermattLock from 'zermatt-core/scss.mjs'
 
 export default defineConfig({
+    plugins: [
+        {
+            name: 'zermatt-lock',
+            async buildStart () {
+                await zermattLock()
+            }
+        }
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern'
+            }
+        }
+    },
     build: {
-        manifest: true
+        manifest: true,
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+                        return 'assets/zermatt.css'
+                    }
+                    return 'assets/[name].[hash][extname]'
+                }
+            }
+        }
     }
 })
