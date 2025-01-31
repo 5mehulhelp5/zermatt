fdcbfa4d920253ed5878d1d355d5bb15fffa20c2
Hervé Guétin <herve.guetin@dnd.fr>
2025-01-31 18:02:05 +0100

    Added CORS to vite.config.js to allow any dev domain to reach to vite server in dev mode

--- view/frontend/web/zermatt/vite.config.js
+++ view/frontend/web/zermatt/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import zermattStyles from 'zermatt-core/server/scss.mjs'
 import zermattWatch from 'zermatt-core/server/watch.mjs'
 
@@ -6,16 +6,21 @@ const args = process.argv.slice(2)
 const withReload = !args.includes('--noreload')
 
 export default defineConfig({
+    server: {
+        cors: true,
+        host: 'localhost',
+        port: 5173
+    },
     plugins: [
         {
             name: 'zermatt-watch',
-            async configureServer (server) {
+            async configureServer(server) {
                 await zermattWatch(server, withReload)
             }
         },
         {
             name: 'zermatt-styles',
-            async buildStart () {
+            async buildStart() {
                 await zermattStyles()
                 //await zermattStyles([path.join(path.dirname(fileURLToPath(import.meta.url)), '/scss/custom-entry-file.scss')]) // Custom SASS
                 //await zermattStyles([]) // Bypass all
